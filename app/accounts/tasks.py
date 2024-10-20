6e793f531f3724094d9397bf3591a797279f355f
ol-smaha <oleh.smaha.work@gmail.com>
2024-10-20 19:53:31 +0300

    upd tasks

--- app/accounts/tasks.py
+++ app/accounts/tasks.py
@@ -1,3 +1,5 @@
+import time
+
 from celery import shared_task
 
 from accounts.models import CustomUser
@@ -6,12 +8,16 @@ from accounts.models import CustomUser
 
 @shared_task(name='Admin send mails')
 def send_daily_message():
-    admins = CustomUser.objects.filter(is_admin=True)
+    admins = CustomUser.objects.filter(is_superuser=True)
     for admin in admins:
         admin.first_name = 'TEST'
         admin.save()
 
 
 @shared_task(name='Test')
-def test():
-    print(' ***** TEST ***** ')
+def test(word):
+    print(word)
+    return f'Result: {word}'
+
+
+
